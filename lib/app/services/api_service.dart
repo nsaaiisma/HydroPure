d6b804b8fc937ce350c676422df029114fd04420
lkman-is <lukman@borneodynamics.com>
2026-05-14 21:18:43 +0700

    update register parrameter

--- lib/app/services/api_service.dart
+++ lib/app/services/api_service.dart
@@ -1,65 +1,33 @@
 import 'dart:convert';
 
-import 'package:http/http.dart'
-    as http;
+import 'package:http/http.dart' as http;
 
 class ApiService {
-
-  static const String baseUrl = "http://127.0.0.1:8000"; // Ganti dengan URL backend Anda
-      //"http://10.0.2.2:8000"; // Gunakan IP ini untuk emulator Android
+  static const String baseUrl =
+      "http://127.0.0.1:2000"; // Ganti dengan URL backend Anda
+  //"http://10.0.2.2:8000"; // Gunakan IP ini untuk emulator Android
 
   /// SEND OTP
-  Future<bool> sendOtp({
-    required String email,
-  }) async {
+  Future<bool> sendOtp({required String email}) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/send-otp"),
 
-    final response =
-        await http.post(
+      headers: {"Content-Type": "application/json"},
 
-      Uri.parse(
-        "$baseUrl/send-otp",
-      ),
-
-      headers: {
-        "Content-Type":
-            "application/json",
-      },
-
-      body: jsonEncode({
-        "email": email,
-      }),
+      body: jsonEncode({"email": email}),
     );
 
     return response.statusCode == 200;
   }
 
   /// VERIFY OTP
-  Future<bool> verifyOtp({
+  Future<bool> verifyOtp({required String email, required String otp}) async {
+    final response = await http.post(
+      Uri.parse("$baseUrl/verify-otp"),
 
-    required String email,
+      headers: {"Content-Type": "application/json"},
 
-    required String otp,
-
-  }) async {
-
-    final response =
-        await http.post(
-
-      Uri.parse(
-        "$baseUrl/verify-otp",
-      ),
-
-      headers: {
-        "Content-Type":
-            "application/json",
-      },
-
-      body: jsonEncode({
-
-        "email": email,
-
-        "otp": otp,
-      }),
+      body: jsonEncode({"email": email, "otp": otp}),
     );
 
     return response.statusCode == 200;
